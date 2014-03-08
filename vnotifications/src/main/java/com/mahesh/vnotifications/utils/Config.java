5f0ae453f71b4b6ab588620d1839dc2c4484d0f2
Mahesh <mahesh@mahesh.ws>
2014-03-08 15:37:08 +0530

    GCM Registration Complete 2

---
+++ vnotifications/src/main/java/com/mahesh/vnotifications/utils/Config.java
@@ -0,0 +1,24 @@
+package com.mahesh.vnotifications.utils;
+
+/**
+ * Created by Mahesh on 3/8/14.
+ */
+public interface Config {
+
+
+    // CONSTANTS
+    static final String DOMAIN_URL ="http://192.168.2.2/vn/";
+    //static final String DOMAIN_URL ="http://vu.mahesh.ws.md-in-5.webhostbox.net/";
+
+    // Google project id
+    static final String GOOGLE_SENDER_ID = "889906571620";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCM Service";
+
+    static final String EXTRA_MESSAGE = "message";
+
+
+}
