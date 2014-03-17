6a055577cc0a46daf2ad11d6d729cb4d7cb5fa63
Mahesh <mahesh@mahesh.ws>
2014-03-17 15:10:53 +0530

    Remove pending notification

--- vnotifications/src/main/java/com/mahesh/vnotifications/HomeActivity.java
+++ vnotifications/src/main/java/com/mahesh/vnotifications/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.mahesh.vnotifications;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -47,6 +48,8 @@ public class HomeActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(1);
     }
 
     @Override
