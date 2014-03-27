77b41dfe3dd73585c16c33aa6e8f258d31e4962e
Mahesh <mahesh@mahesh.ws>
2014-03-27 20:08:37 +0530

    Remove menus from Actionbar

--- vnotifications/src/main/java/com/mahesh/vnotifications/beta/HomeActivity.java
+++ vnotifications/src/main/java/com/mahesh/vnotifications/beta/HomeActivity.java
@@ -157,10 +157,7 @@ public class HomeActivity extends ActionBarActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
